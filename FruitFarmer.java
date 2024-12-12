43a04a6fe93b34d8094069d9139413aec1078be1
cynthiaWEpicCoder <cwen60@stuy.edu>
2024-12-12 16:33:15 -0500

    Create FruitFarmer.java

---
+++ FruitFarmer.java
@@ -0,0 +1,13 @@
+class FruitFarmer implements Farmer{
+  public FruitFarmer(String name){
+    super(name);
+  }
+  public String getCrop(){
+    return "apple";
+  }
+
+  public String getProduce(){
+    return "pie";
+  }
+}
+  
