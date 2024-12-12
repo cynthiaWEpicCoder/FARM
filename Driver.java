5ed6acff18efd6b945f980c5f6fbf1d4fd011c55
ajing60 <audrey.y.jing@gmail.com>
2024-12-12 22:14:22 +0000

    add

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 public class Driver{
     public static void main (String[] args){
         Farmer player1 = new FruitFarmer ("Aud");
+        
         Farmer player2 = new GrainFarmer ("Cyn");
     }
 }
--- VeggieFarmer.java
+++ VeggieFarmer.java
@@ -1,6 +1,9 @@
 class VeggieFarmer implements Farmer{
   public VeggieFarmer(String name){
     super(name);
+    System.out.println ("You start your journey at Nightmare Farm as a veggie farmer. Your goal is to earn 100 coins and escape the farm.");
+    System.out.println ("Each turn, you will have 5 moves. You can use one move to plant one spinach plant, water all of your plants once, harvest one ripe spinach, create one salad, or sell one salad.");
+    System.out.println ("At the end of each turn, a nightmare will happen. Have fun!");
   }
   public String getCrop(){
     return "lettuce";
