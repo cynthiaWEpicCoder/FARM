88e512e2b19dfcd417ca1e9348d65f351572970d
cynthiaWEpicCoder <cwen60@stuy.edu>
2024-12-12 17:03:25 -0500

    Update Farmer.java

--- Farmer.java
+++ Farmer.java
@@ -4,8 +4,10 @@ import java.util.*;
 public abstract class Farmer{
   private String name;
   private int coins;
-  private ArrayList<Integer> inv = ArrayList<Integer>();
+  private ArrayList<Integer> farm = ArrayList<Integer>();
+  private ArrayList<String> inv = ArrayList<String>();
   private String type;
+  private int turns = 5;
 
   /*There is no no-arg constructor. Be careful with your subclass constructors.*/
   
@@ -36,4 +38,48 @@ public abstract class Farmer{
 
   public abstract String getCrop();
   public abstract String getProduce();
+
+  public void plant(){
+    farm.add(0);
+    turns -= 1;
+    System.out.println("You planted one " + this.getCrop() + ".");
+  }
+  
+  public void water(){
+    for (int i = 0; i < farm.getSize(); i++){
+      farm.set(i, farm.get(i)+1);
+    }
+    turns -= 1;
+    System.out.println("You watered all your" + this.getCrop()+ "s.");
+  }
+
+  public void harvest(){
+    int ripeInd = -1;
+    for (int i = 0; i < farm.size(); i++){
+      if (farm.get(i) >= 2){
+        ripeInd = i;
+        break;
+      }
+    }
+    if (ripeInd == -1){
+      System.out.println("You tried to harvest your crops but none were ripe.");
+      turns -= 1;
+    }
+    if (ripeInd != -1){
+      farm.remove(ripeInd);
+      inv.add("Ripe " + this.getCrop());
+      turns -= 1;
+    }
+  }
+
+  public void sell(){
+    int produceInd = -1;
+    for (int i = 0; i< inv.size(); i++){
+      if (inv.get(i).equals(this.getProduce())){
+        produceInd = i;
+        break;
+      }
+    }
+    if (ripeInd != 
+  
 }
