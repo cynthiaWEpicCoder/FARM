083aaeb2246ab14ca60b26a9f17262ccb187cbf3
ajing60 <audrey.y.jing@gmail.com>
2024-12-12 22:17:33 +0000

    add

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 public class Driver{
     public static void main (String[] args){
         Farmer player1 = new FruitFarmer ("Aud");
-        
+        player1.plant();
         Farmer player2 = new GrainFarmer ("Cyn");
     }
 }
