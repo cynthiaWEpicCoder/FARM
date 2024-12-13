12c4dc540278a3eaf5beb591c8c639a329bbaa9e
ajing60 <audrey.y.jing@gmail.com>
2024-12-13 09:05:02 -0500

    add start

--- GrainFarmer.java
+++ GrainFarmer.java
@@ -1,6 +1,7 @@
 class GrainFarmer extends Farmer{
   public GrainFarmer(String name){
     super(name);
+    super.printMsg();
     System.out.println ("You start your journey at Nightmare Farm as a grain farmer. Your goal is to earn 100 coins and escape the farm.");
     System.out.println ("Each turn, you will have 5 moves. You can use one move to plant one grain, water all of your seeds once, harvest one wheat, create one bread, or sell one bread.");
     System.out.println ("At the end of each turn, a nightmare will happen. Have fun!");
