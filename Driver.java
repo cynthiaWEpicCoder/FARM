124b7b685c21ceab1cc406a17ddd9f7f274ced53
cynthiaWEpicCoder <cwen60@stuy.edu>
2024-12-13 09:58:13 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class Driver{
     public static void main (String[] args){
         int day = 0;
-        boolean stonksUp = false;
-        boolean daylightGivings = false;
-
+        Random rand = new Random();
         Scanner input = new Scanner(System.in);
+        
         System.out.println("What is your name?");
         String name = input.nextLine();
         System.out.println ("Hello " + name + ", and welcome to Nightmare Farm! You used to be a student at Stuyvesant High School, but then one day you were taken away. You're not quite sure what happened, but now you're here, and you want to leave as fast as possible.");
@@ -30,6 +29,8 @@ public class Driver{
                 int move = input.nextInt();
                 turn(move);
             }
+            boolean stonksUp = false;
+            boolean daylightGivings = false;
             System.out.println("You are out of moves today. Great day of work! Goodnight.");
             player1.setTurns(5);
         }
@@ -49,11 +50,29 @@ public class Driver{
             player1.produce();
         }
         if (turnType == 5){
-            player1.sell();
+            player1.sell(stonksUp);
         }
     }
 
     public static void nightmare(){
-
-        
+        int nightmareType = rand.nextInt(11) + 1;
+        if (nightmareType == 1){
+            System.out.println("A tornado visits your farm. All of your crops are gone. Your inventory is now size 0.");
+            player1.emptyInventory();
+        }
+        if (nightmareType == 2){
+            System.out.println("Overnight, the stonk market goes up. Tomorrow, your " + player1.getProduce() + " sells for double coins. Hooray!");
+            stonksUp = true;
+        }
+        if (nightmareType == 3){
+            int lossPercent = (rand.nextInt(5)+ 1) * 10;
+            System.out.println("Jim the Mild Demon visits your farm and helps himself to some yummy coins. You lose " + lossPercent+ "% of your coins. Unfortunate.");
+            player1.setCoins(player1.getCoins() * lossPercent*0.01);
+        }
+        if (nightmareType == 4){
+            int lossPercent = (rand.nextInt(5)+ 6) * 10;
+            System.out.println("Jennifer the Spicy Demon visits your farm. You lose " + lossPercent+ "% of your coins. Unfortunate.");
+            player1.setCoins(player1.getCoins() * lossPercent*0.01);
+        }
+    }               
 }
