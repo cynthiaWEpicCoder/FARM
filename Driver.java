d93e23fa9df72a3f829a8923695652a503455759
cynthiaWEpicCoder <cwen60@stuy.edu>
2024-12-12 20:42:08 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -2,6 +2,15 @@ public class Driver{
     public static void main (String[] args){
         Farmer player1 = new FruitFarmer ("Aud");
         player1.plant();
-        Farmer player2 = new GrainFarmer ("Cyn");
+        player1.water();
+        player1.water();
+        System.out.println(player1.getFarm);
+        player1.harvest();
+        System.out.println(player1.getInv());
+        player1.produce();
+        System.out.println(player1.getInv());
+        player1.sell();
+        System.out.println(player1.getInv());
+        System.out.println(player1.getCoins());
     }
 }
