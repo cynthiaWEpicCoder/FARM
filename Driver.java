66d00b6524935b978d81b7fb15c77b6bc6c3e831
cynthiaWEpicCoder <cwen60@stuy.edu>
2024-12-13 09:12:18 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Driver{
     public static void main (String[] args){
         Farmer player1 = new FruitFarmer ("Aud");
@@ -11,6 +13,41 @@ public class Driver{
         System.out.println(player1.getInv());
         player1.sell();
         System.out.println(player1.getInv());
-        System.out.println(player1.getCoins());
+        System.out.println(player1.getCoins());u
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        
+        while (player1.getCoins() < 100){
+            while (player1.getTurns() > 0){
+                System.out.println("What will you do next? \n1 to plant \n2 to water \n3 to harvest \n4 to produce item \n5 to sell item");
+                int move = input.nextInt();
+                turn(move);
+            }
+            System.out.println("You are out of moves today. Great day of work! Goodnight.");
+            player1.setTurns(5);
+        }
     }
+    public static void turn(int turnType){
+        if (turnType == 1){
+            player1.plant();
+        }
+        if (turnType == 2){
+            player1.water();
+        }
+        if (turnType == 3){
+            player1.harvest();
+        }
+        if (turnType == 4){
+            player1.produce();
+        }
+        if (turnType == 5){
+            player1.sell();
+        }
+    }
+
+    public static void nightmare(){
+
+        
 }
